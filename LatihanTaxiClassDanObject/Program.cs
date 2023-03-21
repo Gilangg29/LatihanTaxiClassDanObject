ec96884e429f50e2cd7de8c28896c431995c32c4
unknown <Gilangg29>
2023-03-21 20:31:02 +0700

    first commit

---
+++ LatihanTaxiClassDanObject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxiDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            Class taxi = new Class();
+
+            // pengertian nilai properties
+            taxi.DriverName = "Gilang";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 10;
+
+            // pemanggil method
+
+            taxi.TaxiInfo();
+            if (taxi.OnDuty == true)
+            {
+                taxi.PickUpPassanger();
+            }
+
+            else if (taxi.OnDuty == false)
+            {
+                taxi.DropOffPassanger();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
